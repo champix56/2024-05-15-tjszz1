e0fefd5c1bcff8aa113376cc831b763a7ea3ac63
champix56 <desorbaix@free.fr>
2024-05-15 17:14:03 +0200

    fin jour 1 avec gestion d'etat

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,32 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../ui/Button/Button.tsx";
 import "./App.css";
 
 function App() {
-  const [counter, setCounter] = useState(0)
-
+  const [counter, setCounter] = useState(-10000);
+  useEffect(() => {
+    console.log("declenchement de l'effect didMount & didUpdate", counter);
+    // return () => {
+    //   console.log('démontage')
+    // };
+  }, [counter]);
+  useEffect(() => {
+    setCounter(0);
+  }, []);
   return (
     <div style={{ textAlign: "center" }}>
       <div>counter : {counter}</div>
       <Button
+      style={{padding:'0', }}
         onbuttonclick={(maValue: any) => {
           console.log("buttonclicked from App", maValue);
-          setCounter(counter-1);
-          console.log('====================================');
-          console.log(counter);
-          console.log('====================================');
+          setCounter(counter - 1);
+          // console.log('====================================');
+          // console.log(counter);
+          // console.log('====================================');
         }}
         bgColor="tomato"
       >
         -1
-      </Button>&nbsp;
+      </Button>
+      &nbsp;
       <Button
         onbuttonclick={(maValue: any) => {
           console.log("buttonclicked from App", maValue);
-          setCounter(counter+1);
-          console.log('====================================');
-          console.log(counter);
-          console.log('====================================');
+          setCounter(counter + 1);
+          // console.log('====================================');
+          // console.log(counter);
+          // console.log('====================================');
         }}
       >
         +1
--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import styles from "./Button.module.css";
 interface IButtonProps {
   onbuttonclick?: Function;
   children: string | React.ReactElement | Array<React.ReactNode | string>;
   type?: "submit" | "reset" | "button" | undefined;
+  bgColor?: "skyblue" | "tomato" | "lightgreen";
+  style: object
   //obj?:{a:number;s?:string;o:{aa:string}}
 }
 const Button: React.FC<IButtonProps> = ({
   children,
   onbuttonclick,
+  bgColor = "skyblue",
   type = "button",
+  style
 }) => {
+  const [isClicked, setIsClicked] = useState(true);
+
+  useEffect(() => {
+    setIsClicked(false);
+  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setIsClicked(false);
+  //   }, 270);
+  // }, [isClicked]);
+  
   const clickHander = (
     evt: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
+    setIsClicked(true);
     console.log(evt);
     // if (undefined !== props.onbuttonclick) {
     onbuttonclick!("coucou");
@@ -21,7 +38,14 @@ const Button: React.FC<IButtonProps> = ({
   };
   //console.log(props);
   return (
-    <button onClick={clickHander} className="Button" type={type}>
+    <button
+      onClick={clickHander}
+      className={
+        isClicked ? `${styles.Button} ${styles.clicked}` : styles.Button
+      }
+      type={type}
+      style={{...style, backgroundColor: bgColor }}
+    >
       {children}
     </button>
   );
