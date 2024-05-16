422dfe2fef64e1c19a161f3b03cfc574203edaf3
champix56 <desorbaix@free.fr>
2024-05-17 00:17:56 +0200

    gestion asyncThunk

--- src/store/ressources.ts
+++ src/store/ressources.ts
@@ -1,5 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { ImageInterface, MemeInterface } from "orsys-tjs-meme";
+import { RESSOURCES_NAME, REST_ADR } from "../config/config";
 export interface IRessourcesState {
   memes: Array<MemeInterface>;
   images: Array<ImageInterface>;
@@ -10,12 +11,24 @@ const initialState: IRessourcesState = {
 };
 
 const ressources = createSlice({
-  name: 'ressources',
+  name: "ressources",
   initialState,
-  reducers: {
-  },
+  reducers: {},
 });
 
 // export const {} = ressources.actions;
 
-export  const ressourcesReducer=ressources.reducer;
+export const fetchDatas = createAsyncThunk(
+  "ressources/ressources",
+  async () => {
+    const prImages = await fetch(`${REST_ADR}${RESSOURCES_NAME.images}`);
+    const prMemes = await fetch(`${REST_ADR}${RESSOURCES_NAME.meme}`);
+    const prFull = await Promise.all([prMemes, prImages]);
+    return await {
+      images: await prFull[0].json(),
+      memes: await prFull[1].json(),
+    };
+  }
+);
+
+export const ressourcesReducer = ressources.reducer;
