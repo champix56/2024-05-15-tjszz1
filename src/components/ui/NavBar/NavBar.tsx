8e12e10faafedec623b8c672ec7e58228102d9ce
champix56 <desorbaix@free.fr>
2024-05-16 16:53:27 +0200

    liens avec react router dom avec bootstrap

--- src/components/ui/NavBar/NavBar.tsx
+++ src/components/ui/NavBar/NavBar.tsx
@@ -1,19 +1,30 @@
 import React from "react";
 import { Container, Nav, Navbar as NavBar } from "react-bootstrap";
-import '../../../../node_modules/bootstrap/dist/css/bootstrap.css'
-import '../../../../node_modules/bootstrap/dist/js/bootstrap.js'
+import "../../../../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../../../../node_modules/bootstrap/dist/js/bootstrap.js";
+import FlexH3Grow from "../../layout/FlexH3Grow/FlexH3Grow.js";
+import { Link } from "react-router-dom";
 // import styles from "./NavBar.module.css";
+import { LinkContainer } from "react-router-bootstrap";
 interface INavBarProps {}
 
 const Navbar: React.FC<INavBarProps> = () => {
   return (
     <NavBar bg="dark" data-bs-theme="dark">
       <Container>
-        <NavBar.Brand href="#home">Home</NavBar.Brand>
+        <LinkContainer to="/home">
+          <NavBar.Brand>Home</NavBar.Brand>
+        </LinkContainer>
         <Nav className="me-auto">
-          <Nav.Link href="#home">thumbnail</Nav.Link>
-          <Nav.Link href="#features">new</Nav.Link>
-          <Nav.Link href="#pricing">edit/0</Nav.Link>
+          <LinkContainer to="/thumbnail">
+            <Nav.Link>thumbnail</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/new">
+            <Nav.Link>new</Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/edit/0">
+            <Nav.Link>edit/0</Nav.Link>
+          </LinkContainer>
         </Nav>
       </Container>
     </NavBar>
