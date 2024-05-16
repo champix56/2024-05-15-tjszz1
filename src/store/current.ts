25877602a0757c82be851346c897e64a9463c766
champix56 <desorbaix@free.fr>
2024-05-17 00:31:04 +0200

    gestion du save au rest du current

--- src/store/current.ts
+++ src/store/current.ts
@@ -1,5 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { emptyMeme, MemeInterface } from "orsys-tjs-meme";
+import { RESSOURCES_NAME, REST_ADR } from "../config/config";
 
 const initialState = emptyMeme;
 
@@ -22,6 +23,35 @@ const current = createSlice({
       Object.assign(state, initialState);
     },
   },
+  extraReducers(builder) {
+    builder.addCase(
+      saveCurrent.fulfilled,
+      (s, a: { type: string; payload: MemeInterface }) => {
+        Object.assign(s,a.payload)
+      }
+    );
+  },
 });
+export const saveCurrent = createAsyncThunk(
+  "current/save",
+  async (meme: MemeInterface) => {
+    const prSave = await fetch(
+      `${REST_ADR}${
+        undefined === meme.id
+          ? RESSOURCES_NAME.meme
+          : `${RESSOURCES_NAME.meme}/${meme.id}`
+      }`,
+      {
+        method: undefined === meme.id ? "POST" : "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(meme),
+      }
+    );
+    return await prSave.json();
+  }
+);
 export const { change, clear } = current.actions;
 export const currentReducer = current.reducer;
