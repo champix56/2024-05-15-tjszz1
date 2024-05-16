3d3dea574112063221a15a9b471f69fb51ef28e7
champix56 <desorbaix@free.fr>
2024-05-17 00:24:18 +0200

    subscribe/dispatch avec et sans constructeur d'action

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,15 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import { currentReducer } from "./current.ts";
-import {ressourcesReducer} from './ressources.ts'
+import { change, currentReducer } from "./current.ts";
+import { fetchDatas, ressourcesReducer } from "./ressources.ts";
 export const store = configureStore({
   reducer: combineReducers({
     current: currentReducer,
     ressources: ressourcesReducer,
   }),
 });
+store.subscribe(() => {
+  console.log(store.getState());
+});
+store.dispatch({ type: "current/change", payload: { text: "coucou" } });
+store.dispatch(change({ text: "demat breizh" }));
+store.dispatch(fetchDatas());
