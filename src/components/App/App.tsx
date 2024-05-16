26c06be081a4c1bbb1d79f1d1cfcc192f1d11c20
champix56 <desorbaix@free.fr>
2024-05-17 00:01:03 +0200

    gestions des routes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -12,6 +12,8 @@ import FlexW1Grow from "../layout/FlexW1Grow/FlexW1Grow";
 import Footer from "../ui/Footer/Footer";
 import MemeForm from "../functionnal/MemeForm/MemeForm";
 import { RESSOURCES_NAME, REST_ADR } from "../../config/config";
+import { Route, Routes } from "react-router-dom";
+import Home from "../../pages/Home/Home";
 
 interface IAppProps {}
 
@@ -41,26 +43,40 @@ class App extends Component<IAppProps, IAppState> {
       <FlexH3Grow>
         <Header />
         <NavBar />
-        <FlexW1Grow>
-          <MemeSVGViewer
-            meme={this.state.meme}
-            image={this.state.images.find(
-              (img) => img.id === this.state.meme.imageId
-            )}
-            basePath=""
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route
+            path="/new"
+            element={
+              <FlexW1Grow>
+                <MemeSVGViewer
+                  meme={this.state.meme}
+                  image={this.state.images.find(
+                    (img) => img.id === this.state.meme.imageId
+                  )}
+                  basePath=""
+                />
+                <MemeForm
+                  meme={this.state.meme}
+                  onMemeChange={(meme: MemeInterface) => {
+                    this.setState({ meme: meme });
+                  }}
+                  onMemeSubmit={(m) => {
+                    console.log(
+                      "%c%s",
+                      "color:blue;font-Size:24pt;text-decoration:underline",
+                      "Le button submit a été clicked"
+                    );
+                    //// fetch()
+                  }}
+                  images={this.state.images}
+                />
+              </FlexW1Grow>
+            }
           />
-          <MemeForm
-            meme={this.state.meme}
-            onMemeChange={(meme: MemeInterface) => {
-              this.setState({ meme: meme });
-            }}
-            onMemeSubmit={(m) => {
-              console.log('%c%s','color:blue;font-Size:24pt;text-decoration:underline','Le button submit a été clicked');
-              //// fetch()
-            }}
-            images={this.state.images}
-          />
-        </FlexW1Grow>
+        </Routes>
+
         <Footer />
       </FlexH3Grow>
     );
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Home = (props:any) => {
+    console.log(props)
+  return (
+    <div>
+      <h1>Bonjour et bienvenue</h1>
+      <p>Amusez vous à construire votre meme</p>
+    </div>
+  );
+};
+
+export default Home;
